b70980a8eec9e8743592f0b5c170d39d4791439e
dongthangsnowbody@gmail.com <dongthangsnowbody@gmail.com>
2024-02-20 01:00:43 +0000

    remote state file to s3

--- main.tf
+++ main.tf
@@ -10,7 +10,26 @@ terraform {
       version = "3.10.0"
     }
   }
+  backend "s3" {
+    bucket = "terraform"
+    key = "timo/terraform.tfstate"
+    region = "us-east-1"
+    endpoint = "s3-north1.viettelidc.com.vn"
+    force_path_style            = true
+    skip_metadata_api_check     = true
+    skip_credentials_validation = true
+  }
 }
+
+
+# data "terraform_remote_state" "network" {
+#   backend = "s3"
+#   config = {
+#     bucket = "terraform"
+#     key    = "timo/terraform.tfstate"
+#     region = "us-east-1"
+#   }
+# }
 
 ## Providers definition
 provider "vcd" {
